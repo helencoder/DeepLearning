bca25807ad733cd93f89cf4ccca5d72c1ffbad1d
helencoder <731873664@qq.com>
2018-01-23 15:23:17 +0800

    基于深度学习的分类

---
+++ src/main/java/com/helencoder/classification/CnnClassification.java
@@ -0,0 +1,10 @@
+package com.helencoder.classification;
+
+/**
+ * 基于CNN的文本分类
+ *
+ * Created by helencoder on 2018/1/23.
+ */
+public class CnnClassification {
+    
+}
---
+++ src/main/java/com/helencoder/classification/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 基于深度学习的分类方式归集
+ *
+ * Created by helencoder on 2018/1/23.
+ */
+package com.helencoder.classification;
